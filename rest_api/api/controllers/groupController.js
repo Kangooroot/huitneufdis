395c31e4427707eb9eae4154583b3bacdcde1f00
Kangooroot <hoony@hotmail.fr>
2018-03-15 16:12:24 +0100

    zob

--- rest_api/api/controllers/groupController.js
+++ rest_api/api/controllers/groupController.js
@@ -34,11 +34,16 @@ exports.create_group = function(req, res) {
         let totalWeight = 0;
         let orderGroup = [];
 
+        // On récupère toutes les commandes qui ne sont assignées à aucun préparateur
         Order.find({ preparator: null }).populate("products.product").exec(function(err, orders) {
 
             if(err) res.send(err);
 
+            // Ce tableau d'objets json va associer chaque commande à un barycentre
             let barycentres = [];
+
+            // On inverse les commandes pour les ordonner par ordre de date décroissante
+            orders = orders.reverse();
 
             // On calcule le barycentre de chaque commande
             for(let i = 0; i < orders.length; i++) {
@@ -70,16 +75,16 @@ exports.create_group = function(req, res) {
 
                 let orderWeight = 0;
 
-                // On calcule le poids total de la commande
+                // On calcule le poids total de la commande en cours
                 for(let p = 0; p < orders[i].products.length; p++) {
                     orderWeight += orders[i].products[p].product.weight;
                 }
 
+                // Si ce préparateur peut la prendre, on l'ajoute directement
                 if(orderWeight < preparator.maxWeight) {
                     orderGroup.push(orders[i]._id);
                     totalWeight += orderWeight;
                     firstOrderBarycentre = barycentres[i];
-                    //orders.splice(i, 1);
                 }
             }
 
--- rest_api/server.js
+++ rest_api/server.js
@@ -16,6 +16,12 @@ mongoose.connect('mongodb://localhost/huitneufdis');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 
 require('./api/routes/orderRoutes')(app); //importing routes
